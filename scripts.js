84db4cdd662123752a0ec5a9b078dcab8c61bb4f
Kendrew <32181609+kendrews@users.noreply.github.com>
2017-12-05 03:15:58 -0800

    yes

--- scripts.js
+++ scripts.js
@@ -1,30 +1,24 @@
 var canvas = document.getElementById("canvas");
 var clouds = document.getElementById("clouds");
-var cloud1 = document.getElementById("cloud1");
+var y = window.pageYOffset;
 
 window.onscroll = function(){
 
 
   var y = window.pageYOffset;
 
+  console.log(y);
 
-  cloud1.style.top = 200 + y * -1;
-  cloud2.style.top = 240 + y * -2;
-  cloud3.style.top = 260 + y * -1;
-  cloud4.style.top = 350 + y * -1.5;
-  cloud5.style.top = 350 + y * -1;
-  cloud6.style.top = 450 + y * -2;
-  cloud7.style.top = 550 + y * -1;
-  cloud8.style.top = 650 + y * -1;
-  cloud9.style.top = 750 + y * -1;
-  cloud10.style.top = 850 + y * -2;
-  cloud11.style.top = 950 + y * -1.5;
-  cloud12.style.top = 1050 + y * -2;
-  cloud13.style.top = 1000 + y * -1;
-  cloud14.style.top = 1100 + y * -1.5;
-  cloud15.style.top = 1200 + y * -2;
-  cloud16.style.top = 1350 + y * -2;
-  cloud17.style.top = 1350 + y * -3;
-  cloud18.style.top = 1350 + y * -3;
+  cloud1.style.top = 1000 + y * -2 + "px";
+  cloud2.style.top = 2000 + y * -1.5 + "px";
+  cloud3.style.top = 3000 + y * -1.5 + "px";
+  cloud4.style.top = 4000 + y * -1.5 + "px";
+  cloud5.style.top = 5000 + y * -1 + "px";
+  cloud6.style.top = 6500 + y * -1.5 + "px";
+  cloud7.style.top = 7000 + y * -.5 + "px";
+  cloud8.style.top = 8000 + y * -.5 + "px";
+  cloud9.style.top = 10000 + y * -.5 + "px";
+  cloud10.style.top = 10500 + y * -.5 + "px";
+  cloud11.style.top = 11000 + y * -1 + "px";
 
 };
